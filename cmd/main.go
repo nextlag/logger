02ad354121d84570965ca9355cfa9930ddefa5ce
nextbug <nextbug@ya.ru>
2025-03-18 13:17:44 +0300

    add example main

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bytes"
+	"log"
+	"os"
+
+	"github.com/nextlag/logger/config"
+	"github.com/nextlag/logger/l"
+)
+
+const configFile = "config.json"
+
+func main() {
+	data, err := os.ReadFile(configFile)
+
+	cfg, err := config.Load(bytes.NewReader(data))
+	if err != nil {
+		log.Fatal("failed to load config")
+	}
+
+	logger, err := l.NewLogger(cfg)
+	if err != nil {
+		log.Fatal("failed to initialize logger")
+	}
+
+	logger.Info("Config", "logging", cfg.Logging)
+}
